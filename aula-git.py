0dec12fce2ca4417d6ef0e43603282748fc06eb5
“Diego <diego.spinella@gmail.com>
2025-12-15 21:19:51 -0300

    Teste de subida

---
+++ aula-git.py
@@ -0,0 +1,5 @@
+nome = 'Diego'
+sobrenome = 'Spinella'
+idade = 33
+estado_civil = 'casado'
+print ('O seu nome é {}, com o sobrenome {}, idade {} e estado civil {}'.format(nome, sobrenome, idade, estado_civil))
