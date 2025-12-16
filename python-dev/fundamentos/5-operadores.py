05367d6238fb7da1f1348d3d7e6b2e9521b024cd
“Diego <diego.spinella@gmail.com>
2025-12-16 11:41:22 -0300

    Subida do projeto

---
+++ python-dev/fundamentos/5-operadores.py
@@ -0,0 +1,46 @@
+num1 = int(input('Digite o primeiro número:\n'))
+num2 = int (input('Digite o segundo número:\n'))
+
+
+#aritméticos
+
+sum = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mult = num1 * num2
+mod = num1 % num2
+exp = num1 ** num2
+
+# print (sum)
+# print (sub)
+# print (div)
+# print (mult)
+# print (mod)
+# print (exp)
+
+#print (f'soma:{sum}\n sub: {sub}\n div: {div}\n multi: {mult}\n mod: {mod}\n exp {exp}')
+
+#comparacao
+
+bigger = num1 > num2
+smiller = num1 < num2
+equal = num1 == num2
+different = num1 != num2
+bigger_equal = num1 >= num2
+smaller_equal = num1 <= num2
+
+print (f'Os numeros {num1} e {num2} sao iguais? {equal}\n. \
+         Os numeros {num1} e {num2} sao diferentes? {different}\n. \
+         O numero {num1} é maior que o numero {num2}? {bigger}')
+
+#Atribuicao
+
+num1 += 1 #num1 = num1 + 1
+num1 -= 1 #num1 = num1 - 1
+num1 *= 1 #num1 = num1 * 1
+num1 /= 1 #num1 = num1 / 1
+
+
+
+
+
