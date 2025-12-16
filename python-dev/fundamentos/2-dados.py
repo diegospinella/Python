1fbdec376e23bba269421328152380b56a8a318e
“Diego <diego.spinella@gmail.com>
2025-12-16 11:03:51 -0300

    Novas aulas

---
+++ python-dev/fundamentos/2-dados.py
@@ -0,0 +1,14 @@
+#Pythonflix
+
+name = 'Top Gun Maverick'
+yearLaunch = 2023
+noteMovie = 9.5
+panIncluded = False
+
+print (name, yearLaunch, noteMovie, panIncluded)
+print (type(name))
+print (type(yearLaunch))
+print (type(noteMovie))
+print (type(panIncluded))
+
+
---
+++ python-dev/fundamentos/3-input.py
@@ -0,0 +1,21 @@
+#utilizando o input
+
+name = input('Digite o nome do filme:\n')
+print (type(name))
+
+yearLaunch = input('Digite o ano de lancamento:\n')
+print (type(yearLaunch))
+
+noteMovie = input('Digite a nota do filme:\n')
+print (type(noteMovie))
+
+#informando o tipo de dados
+
+name = str(input('Digite o nome do filme:\n'))
+print (type(name))
+
+yearLaunch = int(input('Digite o ano de lancamento:\n'))
+print (type(yearLaunch))
+
+noteMovie = float(input('Digite a nota do filme:\n'))
+print (type(noteMovie))
