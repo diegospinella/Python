4a8b10f46403934f2c5c7934e136a0288a174d69
“Diego <diego.spinella@gmail.com>
2025-12-16 19:39:05 -0300

    Add itens

---
+++ python-dev/fundamentos/11-tupla.py
@@ -0,0 +1,22 @@
+filmsLista = ('Matrix','Batman','Simpsons',
+              'Palmeiras','Chaves','Cavaleiro dos Zodíacos')
+
+# 1 - Buscar os dois primeiros itens da tupla
+
+print (filmsLista[0:2])
+
+# 2 - Buscar o último item da lista
+
+print (filmsLista[-1])
+
+# 3 - Buscar filmes até uma determinada posicao
+
+print (filmsLista[0:3])
+
+# 4 - Buscar filmes de uma posicao em diante
+
+print (filmsLista[2:])
+
+# 5 - Recuperar um determinado item de uma tupla pelo nome
+
+print(filmsLista.index('Chaves'))
